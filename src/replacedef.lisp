4e09c37568c033ffca0da8baac3d7465535ba4f7
eojebo <eojebo@ubuntu.(none)>
2010-11-29 11:48:29 -0800

    Added specifications for with-stub

--- src/replacedef.lisp
+++ src/replacedef.lisp
@@ -10,6 +10,17 @@
          (setf (symbol-function ',name) ,oldf)
          ,result))))
 
-(defmacro show (x)
-  `(format t "~a --> ~a~%"
-           ',x ,x))
+;; Specifications
+
+(defun f () 5)
+(defun make-double (x) (* 2 x))
+(defun f-plus-one () (1+ (f)))
+(defun double-plus-one (x) (1+ (make-double x)))
+
+(specification-for with-stub
+                   (it "replaces parameter less function definition"
+                       (with-stub (f () 2)
+                         (equal 3 (f-plus-one))))
+                   (it "replaces function definition with parameter"
+                       (with-stub (make-double (x) 4)
+                         (equal 5 (double-plus-one 2)))))
